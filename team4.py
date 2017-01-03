0976aa39e72bb5d5d50aee6c6c763fbb35fdd32b
Jason-Thorn <jason.thorn@mhslead.org>
2017-01-03 14:46:02 -0700

    Update team4.py

--- team4.py
+++ team4.py
@@ -1,3 +1,5 @@
+import random
+
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -7,8 +9,8 @@
 ####
 
 team_name = 'Team 4' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Coin Flip'
+strategy_description = 'Random'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -18,6 +20,12 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
+    move = random.randint(0,1)
+    if move==0:
+        return c
+    else:
+        return b
+    
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,8 +33,6 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
